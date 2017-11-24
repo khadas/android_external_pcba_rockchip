2fa61b2e5f7bcb0b1116403a57bb7d23bf8e056b
Zorro Liu <lyx@rock-chips.com>
2017-11-24 16:43:12 +0800

    gsensor:improve gsensor test and add gsensor calibration
    Change-Id: Ib1070597dac3e13a7bbccff4c94e99022bbf826a
    Signed-off-by: Zorro Liu <lyx@rock-chips.com>

--- gsensor_test.c
+++ gsensor_test.c
@@ -23,25 +23,24 @@
 #include <math.h>
 #include <poll.h>
 #include <pthread.h>
-
 #include <linux/input.h>
+
 #include "gsensor_test.h"
 #include "../../hardware/rockchip/sensor/st/mma8452_kernel.h"              // 声明驱动为 HAL 提供的功能接口. 应该用更加抽象的文件名.
 #include "common.h"
 #include "test_case.h"
 #include "language.h"
 
-
 #define EVENT_TYPE_ACCEL_X          ABS_X
-#define EVENT_TYPE_ACCEL_Y          ABS_Z
-#define EVENT_TYPE_ACCEL_Z          ABS_Y
-#define ACCELERATION_RATIO_ANDROID_TO_HW        (9.80665f / 1000000)
-
+#define EVENT_TYPE_ACCEL_Y          ABS_Y
+#define EVENT_TYPE_ACCEL_Z          ABS_Z
+#define ACCELERATION_RATIO_ANDROID_TO_HW        (9.80665f / 16384)
 #define  CTL_DEV_PATH    "/dev/mma8452_daemon"
 
 static float g_x = 0;
 static float g_y = 0;
 static float g_z = 0;
+
 static int openInput(const char* inputName)
 {
     int fd = -1;
@@ -85,130 +84,142 @@ static int openInput(const char* inputName)
 }
 
 static int  processEvent(int code, int value)
- {
- 
-	 float v;
-	 switch (code) 
-	 {
-	 case EVENT_TYPE_ACCEL_X:
-		 g_x = value * ACCELERATION_RATIO_ANDROID_TO_HW;
-		 //printf("x:%f\n",v);
-		 break;
-	 case EVENT_TYPE_ACCEL_Y:
-		 g_y = value * ACCELERATION_RATIO_ANDROID_TO_HW;
-		 //printf("y:%f\n",v);
-		 break;
-	 case EVENT_TYPE_ACCEL_Z:
-		 g_z = value * ACCELERATION_RATIO_ANDROID_TO_HW;
-		 //printf("z:%f\n",v);
-		 break;
-	 }
- 
-	 return 0;
-  }
+{
+    float v;
 
- static int readEvents(int fd)
- {
-
-	struct input_event  event;
-
-	ssize_t n = read(fd, &event,sizeof(struct input_event));
-	if (n < 0)
-	{
-		printf("gsensor read fail!\n");
-	 	return n;
-	}
-
-
-
-	 int type = event.type;
-	//printf("type:%d\n",type);
-	 if (type == EV_ABS)
-	 {		 // #define EV_ABS 0x03
-	     processEvent(event.code, event.value);
-	   
-	 }
-	
- 
-     return 0;
- }
- 
-
-
- void* gsensor_test(void *argv)
- {
- 	
-	int ret;
-	int fd;
- 	//struct gsensor_msg *g_msg =  (struct gsensor_msg *)malloc(sizeof(struct gsensor_msg));
-        struct gsensor_msg g_msg;
-	struct testcase_info *tc_info = (struct testcase_info*)argv;
-		
-	/*remind ddr test*/
-	if(tc_info->y <= 0)
-		tc_info->y  = get_cur_print_y();	
-
-	g_msg.y = tc_info->y;
-	ui_print_xy_rgba(0,g_msg.y,255,255,0,255,"%s:[%s..] \n",PCBA_GSENSOR,PCBA_TESTING);
-	tc_info->result = 0;
-        /*
- 	if(!g_msg)
-	{
-		printf("malloc for wlan_msg fail!\n");
-	}
-	else
-	{
-		g_msg->result = -1;
-		//g_msg->y = get_cur_print_y();
-	}
-        */
-        
- 	fd = openInput("gsensor");
-	if(fd < 0)
-	{
-		ui_print_xy_rgba(0,g_msg.y,255,0,0,255,"%s:[%s]\n",PCBA_GSENSOR,PCBA_FAILED);
-		g_msg.result = -1;
-		tc_info->result = -1;
-		return argv;
-	}
-	
-	int fd_dev = open(CTL_DEV_PATH, O_RDONLY);
-    if(fd_dev<0)
+    switch (code) 
     {
-     	printf("opne gsensor demon fail\n");
-		ui_print_xy_rgba(0,g_msg.y,255,0,0,255,"%s:[%s]\n",PCBA_GSENSOR,PCBA_FAILED);
-		g_msg.result = -1;
-		tc_info->result = -1;
-                close(fd);
-		return argv;
-	
+        case EVENT_TYPE_ACCEL_X:
+            g_x = value * ACCELERATION_RATIO_ANDROID_TO_HW;
+            break;
+        case EVENT_TYPE_ACCEL_Y:
+            g_y = value * ACCELERATION_RATIO_ANDROID_TO_HW;
+            break;
+        case EVENT_TYPE_ACCEL_Z:
+            g_z = value * ACCELERATION_RATIO_ANDROID_TO_HW;
+            break;
     }
+
+    return 0;
+}
+
+static int readEvents(int fd)
+{
+    int i;
+    struct input_event  event;
+
+    for (i=0; i<6; i++) {
+        ssize_t n = read(fd, &event,sizeof(struct input_event));
+        if (n < 0)
+        {
+            printf("gsensor read fail!\n");
+            return n;
+        }
+
+        int type = event.type;
+        if (type == EV_ABS)
+            processEvent(event.code, event.value);
+    }
+
+    return 0;
+}
+
+void* gsensor_test(void *argv)
+{
+    int do_calibration;
+    int is_calibration = 0;
+    int tempFd;
+    int ret;
+    int fd;
+    short delay = 20; // 20ms
+    struct gsensor_msg g_msg;
+    struct testcase_info *tc_info = (struct testcase_info*)argv;
+
+    /*remind ddr test*/
+    if (tc_info->y <= 0)
+        tc_info->y  = get_cur_print_y();	
+
+    g_msg.y = tc_info->y;
+    ui_print_xy_rgba(0,g_msg.y,255,255,0,255,"%s:[%s..] \n",PCBA_GSENSOR,PCBA_TESTING);
+    tc_info->result = 0;
+
+    fd = openInput("gsensor");
+    if (fd < 0)
+    {
+        ui_print_xy_rgba(0,g_msg.y,255,0,0,255,"%s:[%s]\n",PCBA_GSENSOR,PCBA_FAILED);
+        g_msg.result = -1;
+        tc_info->result = -1;
+        return argv;
+    }
+
+    int fd_dev = open(CTL_DEV_PATH, O_RDONLY);
+    if (fd_dev<0)
+    {
+        printf("open gsensor demon fail\n");
+        ui_print_xy_rgba(0, g_msg.y, 255, 0, 0, 255, "%s:[%s]\n", PCBA_GSENSOR, PCBA_FAILED);
+        g_msg.result = -1;
+        tc_info->result = -1;
+        close(fd);
+        return argv;
+    }
+
+    ret = ioctl(fd_dev, GSENSOR_IOCTL_APP_SET_RATE, &delay);
+    if (ret < 0)
+    {
+        printf("set sensor rate fail!\n");
+        ui_print_xy_rgba(0, g_msg.y, 255, 0, 0, 255, "%s:[%s]\n", PCBA_GSENSOR, PCBA_FAILED);
+        g_msg.result = -1;
+        tc_info->result = -1;
+        close(fd_dev);
+        close(fd);
+        return argv;
+    }
+
     ret = ioctl(fd_dev, MMA_IOCTL_START);
-    if(ret < 0)
+    if (ret < 0)
     {
-		printf("start sensor fail!\n");
-		ui_print_xy_rgba(0,g_msg.y,255,0,0,255,"%s:[%s]\n",PCBA_GSENSOR,PCBA_FAILED);
-		g_msg.result = -1;
-		tc_info->result = -1;
-                close(fd_dev);
-                close(fd);
-		return argv;
+        printf("start sensor fail!\n");
+        ui_print_xy_rgba(0, g_msg.y, 255, 0, 0, 255, "%s:[%s]\n", PCBA_GSENSOR, PCBA_FAILED);
+        g_msg.result = -1;
+        tc_info->result = -1;
+        close(fd_dev);
+        close(fd);
+        return argv;
     }
 
-	for(;;)
-	{
-		readEvents(fd);
-		//ui_print_xy_rgba(0,g_msg.y,0,255,0,255,"%s:[%s] { %2d,%2d,%2d }\n",PCBA_GSENSOR,PCBA_SECCESS,(int)g_x,(int)g_y,(int)g_z);
-		ui_display_sync(0,g_msg.y,0,255,0,255,"%s:[%s] { %2d,%2d,%2d }\n",PCBA_GSENSOR,PCBA_SECCESS,(int)g_x,(int)g_y,(int)g_z);
-		//ui_print_xy_rgba(0,g_msg->y,0,0,255,255,"gsensor x:%f y:%f z:%f\n",g_x,g_y,g_z);
-		usleep(100000);
-	}
+    tempFd = open("/sys/class/sensor_class/accel_calibration", O_RDWR);
+    if (tempFd > 0)
+        printf("open /sys/class/sensor_class/accel_calibration success\n");
 
+    script_fetch("gsensor", "calibrate", &do_calibration, 1);
+    printf("gsensor do calibration = %d\n", do_calibration);
+    for (;;)
+    {
+        readEvents(fd);
+        if (tempFd && do_calibration && !is_calibration) {
+            char c = '1';
+            ret = write(tempFd, &c, 1);
+            if (ret >= 0) {
+                printf("gsensor calibrate success\n");
+                is_calibration = 1;
+            } else {
+                printf("gsensor calibrate failed, ret = %d, %s\n", ret, strerror(errno));
+            }
+        }
+        if (do_calibration)
+            ui_print_xy_rgba(0, g_msg.y, is_calibration?0:255, is_calibration?255:0, 0, 255, "%s:[%s]   %s[%s] %4f %4f %4f\n",
+                                          PCBA_GSENSOR, PCBA_SECCESS, PCBA_GSENSOR_CALIBRATE,
+                                          is_calibration ? PCBA_SECCESS: PCBA_FAILED, g_x, g_y, g_z);
+        else
+            ui_print_xy_rgba(0, g_msg.y, 0, 255, 0, 255, "%s:[%s] %4f %4f %4f\n", PCBA_GSENSOR, PCBA_SECCESS, g_x, g_y, g_z);
+        usleep(1000000);
+    }
+    if (tempFd)
+        close(tempFd);
     close(fd);
     close(fd_dev);
-	
-    ui_print_xy_rgba(0,g_msg.y,0,255,0,255,"%s:[%s]\n",PCBA_GSENSOR,PCBA_SECCESS);
-	tc_info->result = 0;
-	return argv;
- }
- 
+
+    ui_print_xy_rgba(0, g_msg.y, 0, 255, 0, 255, "%s:[%s]\n", PCBA_GSENSOR, PCBA_SECCESS);
+    tc_info->result = 0;
+    return argv;
+}
